a688f37f2ebc377b616681f17e12c84a9d9c9f3e
alexandresvifpb <alexandreufcg@gmail.com>
2020-03-25 19:20:49 -0300

    Front-end do App

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
@@ -8,36 +9,3 @@ app.use(express.json());
 app.use(routes);
 
 app.listen(3333);
-
-/**
- * Route / Resources
- */
-
- /**
-  * HTTP Methods
-  * 
-  * GET: Fetch a backend information
-  * POST: Create a backend information
-  * PUT: Change a backend information
-  * DELETE: Delete a backend information
-  */
-
-  /**
-   * Parameter types:
-   * 
-   * Query Params: Named parameters sent on the route after "?" (Filters, pagination)
-   * Route Params: Parameters used to identify resources
-   * Request Body: Request body, used to create or change resources
-   */
-
-   /**
-    * SQL: MySQL, SQLite, PostgreSQL, Oracle, Microsoft SQL Server
-    * NoSQL: MongoDB, CouchDB, MariaDB, etc
-    */
-
-    /**
-     * Driver: SELECT * FROM users
-     * Query Builder 
-     */
-
-
